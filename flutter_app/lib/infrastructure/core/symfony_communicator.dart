1e98d3a950640e046ede803241dfa5d6353ede7c
Daniel Lenhardt <ckwdani@gmail.com>
2020-10-04 19:03:16 +0200

    add encoding option to put and post in communicator

--- flutter_app/lib/infrastructure/core/symfony_communicator.dart
+++ flutter_app/lib/infrastructure/core/symfony_communicator.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:dartz/dartz.dart';
 import 'package:flutter_frontend/domain/core/errors.dart';
 import 'package:http/http.dart';
 
@@ -25,16 +28,18 @@ class SymfonyCommunicator{
   ///throws [NotAuthenticatedError], [NotAuthorizedError], [NotFoundError], [InternalServerError]
   ///the id (if needed) should be in the uri
   ///uri has to start with an backslash "/"
-  Future<Response> post(String uri, dynamic body) async{
-    return _handleErrors( () async {client.post(url+uri, headers:header, body: body); });
+  Future<Response> post(String uri, dynamic body, [Encoding encoding]) async{
+    encoding = encoding == null ? "text/plain" : encoding;
+    return _handleErrors( () async {client.post(url+uri, headers:header, body: body, encoding: encoding); });
   }
 
   ///put an resource with uri
   ///throws [NotAuthenticatedError], [NotAuthorizedError], [NotFoundError], [InternalServerError]
   ///the id (if needed) should be in the uri
   ///uri has to start with an backslash "/"
-  Future<Response> put(String uri, dynamic body) async{
-    return _handleErrors( () async {client.put(url+uri, headers:header, body: body); }) ;
+  Future<Response> put(String uri, dynamic body, [Encoding encoding]) async{
+    encoding = encoding == null ? "text/plain" : encoding;
+    return _handleErrors( () async {client.put(url+uri, headers:header, body: body, encoding: encoding); }) ;
   }
 
   ///delete resource with uri

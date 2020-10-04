7e40048896358a9ab5cc3e5674c9d7d4f8d22f10
Toni <toni.boeswetter@outlook.de>
2020-10-04 22:13:41 +0200

    fixed toms brace

--- flutter_app/lib/presentation/pages/login_screen.dart
+++ flutter_app/lib/presentation/pages/login_screen.dart
@@ -6,16 +6,17 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: Text("Weather Search"),
-    ),
-    body: Container(
-      padding: EdgeInsets.symmetric(vertical: 16),
-      alignment: Alignment.center,
-      child: BlocBuilder<SignInFormCubit, SignInFormState>(
-        // ignore: missing_return
-        builder: (context, state) {},
       ),
-    ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 16),
+        alignment: Alignment.center,
+        child: BlocBuilder<SignInFormCubit, SignInFormState>(
+          // ignore: missing_return
+          builder: (context, state) {},
+        ),
+      ),
     );
   }
+}

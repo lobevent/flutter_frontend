ef799a79fcd91e6114c0e6459411c9cd147aed6d
Toni <toni.boeswetter@outlook.de>
2020-10-04 22:11:06 +0200

    made some validators for, singleline check, datecheck and maxlength check
    dont know if the datecheck is  fucked up

--- flutter_app/lib/domain/core/failures.dart
+++ flutter_app/lib/domain/core/failures.dart
@@ -2,6 +2,14 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 
 part 'failures.freezed.dart';
+/*
+@freezed
+abstract class ValueFailure<T> with _$ValueFailure<T> {
+  const factory ValueFailure.auth(AuthValueFailure<T> f) = _Auth<T>;
+  const factory ValueFailure.event(EventValueFailure<T> f) = _Event<T>;
+}
+
+ */
 
 @freezed
 abstract class ValueFailure<T> with _$ValueFailure<T> {
@@ -11,4 +19,26 @@ abstract class ValueFailure<T> with _$ValueFailure<T> {
   const factory ValueFailure.shortPassword({
     @required T failedValue,
   }) = ShortPassword<T>;
+  const factory ValueFailure.exceedingLenght({
+    @required T failedValue,
+  }) = ShortPassword<T>;
+  const factory ValueFailure.invalidDate({
+    @required T failedValue,
+  }) = ShortPassword<T>;
+  const factory ValueFailure.multiline({
+    @required T failedValue,
+  }) = ShortPassword<T>;
 }
+
+/*
+@freezed
+abstract class EventValueFailure<T> with _$EventValueFailure<T> {
+  const factory EventValueFailure.invalidEmail({
+    @required T failedValue,
+  }) = InvalidEmail<T>;
+  const factory EventValueFailure.shortPassword({
+    @required T failedValue,
+  }) = ShortPassword<T>;
+}
+
+ */
--- flutter_app/lib/domain/core/value_validators.dart
+++ flutter_app/lib/domain/core/value_validators.dart
@@ -1,8 +1,6 @@
 import 'package:dartz/dartz.dart';
 
-
 import 'failures.dart';
-
 
 Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   // Maybe not the most robust way of email validation but it's good enough
@@ -21,5 +19,35 @@ Either<ValueFailure<String>, String> validatePassword(String input) {
     return right(input);
   } else {
     return left(ValueFailure.shortPassword(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateLength(
+    String input, int maxLength) {
+  // check if the string is longer than maxlength
+  if (input.length >= maxLength) {
+    return left(ValueFailure.exceedingLenght(failedValue: input));
+  } else {
+    return right(input);
+  }
+}
+
+Either<ValueFailure<String>, String> validateSingleLine(String input) {
+  // check if for example the title of event is a one liner
+  if (input.contains('\n')) {
+    return left(ValueFailure.multiline(failedValue: input));
+  } else {
+    return right(input);
+  }
+}
+
+Either<ValueFailure<String>, String> validateDate(String input) {
+  // check if the date isin valid format
+  //TODO: idk if this is right, might be fucked up somehow
+  DateTime validDate = DateTime.parse(input);
+  if (validDate.isAfter(DateTime.now())) {
+    return right(input);
+  } else {
+    return left(ValueFailure.invalidDate(failedValue: input));
   }
 }

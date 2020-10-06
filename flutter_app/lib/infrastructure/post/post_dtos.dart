5162e1918bb5cb6300700e80eba532785bb6e61c
Daniel Lenhardt <ckwdani@gmail.com>
2020-10-06 22:53:00 +0200

    add profile conversion in post dto

--- flutter_app/lib/infrastructure/post/post_dtos.dart
+++ flutter_app/lib/infrastructure/post/post_dtos.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_frontend/domain/core/value_validators.dart';
 import 'package:flutter_frontend/domain/event/event.dart';
+import 'package:flutter_frontend/domain/post/comment.dart';
 import 'package:flutter_frontend/domain/post/value_objects.dart';
 import 'package:flutter_frontend/domain/profile/profile.dart';
 import 'package:flutter_frontend/infrastructure/event/event_dtos.dart';
+import 'package:flutter_frontend/infrastructure/profile/profile_dtos.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter_frontend/domain/post/post.dart';
 import 'package:flutter/cupertino.dart';
@@ -18,7 +20,7 @@ abstract class PostDto implements _$PostDto {
     @required int id,
     @required DateTime creationDate,
     @required String postContent,
-    @required String owner,
+    @required ProfileDto owner,
     @required EventDto event,
   }) = _PostDto;
 
@@ -27,7 +29,7 @@ abstract class PostDto implements _$PostDto {
       id: post.id,
       creationDate: post.creationDate,
       postContent: post.postContent.getOrCrash(),
-      owner: post.owner, // TODO this needs a string as argument (see unnamed constructor above) but post.owner is of type profile as can be seen in the file: flutter_app/lib/domain/post/post.dart line 19 -> check types
+      owner: ProfileDto.fromDomain(post.owner),
       event: EventDto.fromDomain(post.event),
     );
   }
@@ -41,7 +43,8 @@ abstract class PostDto implements _$PostDto {
       creationDate: creationDate,
       postContent: PostContent(postContent),
       owner: Profile(id: 0, name: null), //TODO
-      event: Event(event),
+      event: event.toDomain(),
+      comments: List<Comment>()
     );
   }
 }

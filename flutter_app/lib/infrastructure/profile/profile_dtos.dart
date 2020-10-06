137e262ce9aa84bab33b52cf6d92895aa6e01b9a
Toni <toni.boeswetter@outlook.de>
2020-10-06 22:09:42 +0200

    profile dtos, dont know it its stable because of string is not a typee bug

---
+++ flutter_app/lib/infrastructure/profile/profile_dtos.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_frontend/domain/profile/profile.dart';
+import 'package:flutter_frontend/domain/profile/value_objects.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'profile_dtos.freezed.dart';
+part 'profile_dtos.g.dart';
+
+@freezed
+abstract class ProfileDto with _$ProfileDto {
+  const ProfileDto._();
+
+  const factory ProfileDto({
+    @required int id,
+    @required String name,
+  }) = _ProfileDto;
+
+  factory ProfileDto.fromDomain(Profile profile) {
+    return ProfileDto(
+      id: profile.id,
+      name: profile.name.getOrCrash(),
+    );
+  }
+
+  factory ProfileDto.fromJson(Map<String, dynamic> json) =>
+      _$ProfileDtoFromJson(json);
+
+  Profile toDomain() {
+    return Profile(
+      id: id,
+      name: ProfileName(name),
+    );
+  }
+}

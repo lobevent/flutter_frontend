c301ec16e337625d5e7b15d64002fba4c9e5a9bf
Daniel Lenhardt <ckwdani@gmail.com>
2020-10-07 15:51:55 +0200

    add getters to remote event service

--- flutter_app/lib/infrastructure/event/event_remote_service.dart
+++ flutter_app/lib/infrastructure/event/event_remote_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:core';
 
 import 'package:flutter_frontend/infrastructure/core/symfony_communicator.dart';
 import 'package:flutter_frontend/infrastructure/event/event_dtos.dart';
@@ -7,6 +8,9 @@ import 'package:http/http.dart';
 class EventRemoteService{
   final String _EVENTBYIDPATH = "/event/{{id}}";
   final String _OWNEDEVENTSPATH = "/user/events";
+  final String _PROFILEEVENTPATH = "/user/events";
+  final String _ATTENDINGEVENTSPATH = "/user/eventStatus";
+  final String _UNREACTEDEVENTSPATH = "/event";
 
 
   SymfonyCommunicator client;
@@ -24,13 +28,40 @@ class EventRemoteService{
   }
 
   Future<List<EventDto>> getOwnedEvents() async{
-    Response response = await client.get(_OWNEDEVENTSPATH);
+    return _getEventList(_OWNEDEVENTSPATH);
+  }
+
+  Future<List<EventDto>> getAttendingEvents() async{
+    return _getEventList(_ATTENDINGEVENTSPATH);
+  }
+
+  Future<List<EventDto>> getUnreactedEvents() async{
+    return _getEventList(_UNREACTEDEVENTSPATH);
+  }
+
+  Future<>
+
+
+
+
+
+
+
+
+
+  Future<List<EventDto>> _getEventList(String path) async{
+    Response response = await client.get(path);
     List<EventDto> events= (jsonDecode(response.body) as List<Map<String, dynamic>>)
         .map((e) => EventDto.fromJson(e)).toList();
 
     return events;
   }
 
-//  Future<EventDto> create
+
+//  Future<List<EventDto>> getViewableEventsFromProfile(int ProfileId){
+//
+//  }
+
+
 
 }

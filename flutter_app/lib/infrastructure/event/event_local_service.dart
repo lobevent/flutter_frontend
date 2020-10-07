0aae5d90379114a24908a8038c31b00333ec8623
prantto <t.prantz@gmx.de>
2020-10-07 14:54:35 +0200

    commented some stuff

--- flutter_app/lib/infrastructure/event/event_local_service.dart
+++ flutter_app/lib/infrastructure/event/event_local_service.dart
@@ -1,5 +1,6 @@
 //Two options: Database or as JSON File:
 //Hive recommanded by resocoder: example https://www.youtube.com/watch?v=R1GSrrItqUs
+//or moor database (sqllite for flutter) reso has also videos
 //JSON in Localstorage: https://rickyanto.com/cache-locally-on-flutter-with-localstorage/
 
 

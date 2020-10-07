14c926b49e5ca85ec5cec2720c4bc0528bb8eafb
Toni <toni.boeswetter@outlook.de>
2020-10-07 14:53:43 +0200

    just created the dart files for auth in infrasstructure layer

---
+++ flutter_app/lib/infrastructure/auth/authenticator_service.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_frontend/domain/auth/auth_failure.dart';
+import 'package:flutter_frontend/domain/auth/i_auth_facade.dart';
+import 'package:flutter_frontend/domain/auth/value_objects.dart';
+
+class AuthenticatorSymfony implements IAuthFacade {
+  final String _authenticator;
+
+  AuthenticatorSymfony(this._authenticator);
+
+  @override
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword(
+      {@required EmailAddress emailAddress, @required Password password}) {
+    // TODO: implement registerWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword(
+      {@required EmailAddress emailAddress, @required Password password}) {
+    // TODO: implement signInWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithGoogle() {
+    // TODO: implement signInWithGoogle
+    throw UnimplementedError();
+  }
+}

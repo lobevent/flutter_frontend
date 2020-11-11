571a7eda803f61e99039e50562d094dfa325ebf0
Toni <toni.boeswetter@outlook.de>
2020-11-11 21:42:51 +0100

    value objects error maxlength fixed v2

--- flutter_app/lib/domain/post/value_objects.dart
+++ flutter_app/lib/domain/post/value_objects.dart
@@ -10,14 +10,12 @@ class PostContent extends ValueObject<String> {
   final Either<ValueFailure<String>, String> value;
 
   // TODO maxLength is never initialized?! -> export constants into a file in domain/core/constants.dart
-  static int maxLength=Constants.maxLength;
+  static int maxLength = Constants.maxLength;
   //check with maxLength (which u get from backend) if the PostContent is too long
   factory PostContent(String input) {
     assert(input != null);
     {
-      return PostContent._(
-        validateLength(input, maxLength: maxLength)
-      );
+      return PostContent._(validateLength(input, maxLength: maxLength));
     }
   }
   const PostContent._(this.value);
@@ -28,14 +26,12 @@ class CommentContent extends ValueObject<String> {
   final Either<ValueFailure<String>, String> value;
 
   // TODO maxLength is never initialized?! -> export constants into a file in domain/core/constants.dart
-  static int maxLength=Constants.maxLength;
+  static int maxLength = Constants.maxLength;
   //check with maxLength (which u get from backend) if the CommentContent is too long
   factory CommentContent(String input) {
     assert(input != null);
     {
-      return CommentContent._(
-        validateLength(input, maxLength:maxLength)
-      );
+      return CommentContent._(validateLength(input, maxLength: maxLength));
     }
   }
   const CommentContent._(this.value);

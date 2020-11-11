4b1acc306c3d6dd567b4b56e87ed45d17ed2e792
Toni <toni.boeswetter@outlook.de>
2020-11-11 21:42:36 +0100

    value objects error maxlength fixed v2

--- flutter_app/lib/domain/post/value_objects.dart
+++ flutter_app/lib/domain/post/value_objects.dart
@@ -3,19 +3,20 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_frontend/domain/core/failures.dart';
 import 'package:flutter_frontend/domain/core/value_objects.dart';
 import 'package:flutter_frontend/domain/core/value_validators.dart';
+import 'package:flutter_frontend/domain/core/constants.dart';
 
 class PostContent extends ValueObject<String> {
   @override
   final Either<ValueFailure<String>, String> value;
 
   // TODO maxLength is never initialized?! -> export constants into a file in domain/core/constants.dart
-  static int maxLength;
+  static int maxLength=Constants.maxLength;
   //check with maxLength (which u get from backend) if the PostContent is too long
   factory PostContent(String input) {
     assert(input != null);
     {
       return PostContent._(
-        validateLength(input, maxLength)
+        validateLength(input, maxLength: maxLength)
       );
     }
   }
@@ -27,13 +28,13 @@ class CommentContent extends ValueObject<String> {
   final Either<ValueFailure<String>, String> value;
 
   // TODO maxLength is never initialized?! -> export constants into a file in domain/core/constants.dart
-  static int maxLength;
+  static int maxLength=Constants.maxLength;
   //check with maxLength (which u get from backend) if the CommentContent is too long
   factory CommentContent(String input) {
     assert(input != null);
     {
       return CommentContent._(
-        validateLength(input, maxLength)
+        validateLength(input, maxLength:maxLength)
       );
     }
   }

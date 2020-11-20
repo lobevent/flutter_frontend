4ae175f41ebd8e446a49b8bcb9b7b0c40b93b0d4
Uli Prantz <prantzuli@gmail.com>
2020-11-21 00:52:57 +0100

    BaseDto is superclass of all Dtos - Fixed event interface

---
+++ flutter_app/lib/infrastructure/core/base_dto.dart
@@ -0,0 +1,29 @@
+abstract class BaseDto<T> {
+  const BaseDto(); // for freezed to work we need this const constructor
+
+   Map<String, dynamic> toJson();
+
+   T toDomain();
+}
+
+
+// Below you can see a minimal example implementation using the BaseDto as superclass
+
+// import 'package:freezed_annotation/freezed_annotation.dart';
+
+// part 'test_dto.freezed.dart';
+// part 'test_dto.g.dart';
+
+// @freezed
+// abstract class TestDto extends BaseDto<User> implements _$TestDto {
+//   const TestDto._();
+
+//   const factory TestDto({
+//     @required String someTestString,
+//   }) = _TestDto;
+
+//   factory TestDto.fromJson(Map<String, dynamic> json) => _$TestDtoFromJson(json);
+
+//   @override
+//   User toDomain() => User();
+// }

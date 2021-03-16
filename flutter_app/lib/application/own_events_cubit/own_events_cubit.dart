482f0c93e24be49dca8ae883aecdafe6d4d65165
Jumpy98 <toni.boeswetter@outlook.de>
2021-03-16 13:10:28 +0100

    ownevents cubit getting the repository to work

--- flutter_app/lib/application/own_events_cubit/own_events_cubit.dart
+++ flutter_app/lib/application/own_events_cubit/own_events_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_frontend/domain/event/event_failure.dart';
 import 'package:flutter_frontend/domain/event/i_event_repository.dart';
-import 'package:flutter_frontend/domain/post/i_comment_repository.dart';
-import 'package:flutter_frontend/domain/post/i_post_repository.dart';
+import 'package:flutter_frontend/infrastructure/event/event_dtos.dart';
 import 'package:flutter_frontend/infrastructure/event/event_local_service.dart';
 import 'package:flutter_frontend/infrastructure/event/event_remote_service.dart';
 import 'package:flutter_frontend/infrastructure/event/event_repository.dart';
@@ -29,7 +29,7 @@ class OwnEventsCubit extends Cubit<OwnEventsState> {
     try {
       emit(OwnEventsState.loading());
       final ownEventsList = await repository.getList(Operation.owned, DateTime.now(), 5);
-      emit(OwnEventsState.loaded(events: ownEventsList));
+      emit(OwnEventsState.loaded(events: ownEventsList.fold((l) => throw EventFailure, (r) => r)));
     } catch (e) {
       emit(OwnEventsState.error());
     }

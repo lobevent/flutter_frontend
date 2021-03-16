2fe748f8d3d02f1739f47d12271757d39c52dfb8
Jumpy98 <toni.boeswetter@outlook.de>
2021-03-16 12:58:00 +0100

    ownevents cubit getting the repository to work

--- flutter_app/lib/application/own_events_cubit/own_events_cubit.dart
+++ flutter_app/lib/application/own_events_cubit/own_events_cubit.dart
@@ -1,6 +1,14 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_frontend/domain/event/i_event_repository.dart';
+import 'package:flutter_frontend/domain/post/i_comment_repository.dart';
+import 'package:flutter_frontend/domain/post/i_post_repository.dart';
+import 'package:flutter_frontend/infrastructure/event/event_local_service.dart';
+import 'package:flutter_frontend/infrastructure/event/event_remote_service.dart';
+import 'package:flutter_frontend/infrastructure/event/event_repository.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter_frontend/domain/event/event.dart';
+import 'package:flutter_frontend/domain/event/i_event_repository.dart';
+import '';
 
 
 
@@ -9,17 +17,19 @@ part 'own_events_state.dart';
 part 'own_events_cubit.freezed.dart';
 
 class OwnEventsCubit extends Cubit<OwnEventsState> {
-  OwnEventsCubit({this.repository}) : super(OwnEventsState.initial()) {
-    _getOwnEvents();
+  OwnEventsCubit() : super(OwnEventsState.initial()) {
+    var repository = EventRepository(EventRemoteService(), EventLocalService());
+    emit(OwnEventsState.initial());
   }
-  final OwnEventsRepository repository;
+  EventRepository repository;
+
 
   Future<void> _getOwnEvents() async {
-    final List<Event> ownEventsList = null;
+    List<Event> ownEventsList = null;
     try {
       emit(OwnEventsState.loading());
-      final movies = await repository.getOwnEvents();
-      emit(OwnEventsState.loaded(event: ownEventsList));
+      final ownEventsList = await repository.getList(Operation.owned, DateTime.now(), 5);
+      emit(OwnEventsState.loaded(events: ownEventsList));
     } catch (e) {
       emit(OwnEventsState.error());
     }

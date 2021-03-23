e47c6a20a263633af2b713d538447ffe87a00458
Daniel Lenhardt <ckwdani@gmail.com>
2021-03-23 16:06:33 +0100

    commented out the tests

--- flutter_app/lib/presentation/routes/router.dart
+++ flutter_app/lib/presentation/routes/router.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter_frontend/presentation/pages/Event/Event_Form/event_form.dart';
 import 'package:flutter_frontend/presentation/pages/feed.dart';
+import 'package:flutter_frontend/presentation/pages/login/login_screen.dart';
 import 'package:flutter_frontend/presentation/pages/own_events_screen.dart';
 
 @MaterialAutoRouter(
@@ -8,7 +9,8 @@ import 'package:flutter_frontend/presentation/pages/own_events_screen.dart';
   routes: <AutoRoute>[
     AutoRoute(page: FeedScreen, initial: true),
     AutoRoute(page: EventFormPage),
-    AutoRoute(page: OwnEventsScreen)
+    AutoRoute(page: OwnEventsScreen),
+    AutoRoute(page: LoginScreen)
   ],
 )
 class $Router{}

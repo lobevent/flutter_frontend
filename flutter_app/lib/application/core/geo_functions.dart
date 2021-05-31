0b690ae0a31bc91d8c964adc2f9a67ae908532cc
prantto <t.prantz@gmx.de>
2021-06-01 01:50:29 +0200

    added geo_functions

---
+++ flutter_app/lib/application/core/geo_functions.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_frontend/infrastructure/core/position.dart';
+import 'package:geolocator/geolocator.dart';
+
+class GeoFunctions{
+  Future<Position> getLocation() async{
+    final Position location = await determinePosition();
+    // final double latitude = location.latitude;
+    // final double longitude = location.longitude;
+    return location;
+  }
+}
+

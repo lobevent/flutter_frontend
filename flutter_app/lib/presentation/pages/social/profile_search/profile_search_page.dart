e296b0811241cce13db46efe355e687ffd6dc4cc
Jumpy98 <toni.boeswetter@outlook.de>
2021-06-10 18:22:11 +0200

    working on profile search, fixed and deleted some shtv2

--- flutter_app/lib/presentation/pages/social/profile_search/profile_search_page.dart
+++ flutter_app/lib/presentation/pages/social/profile_search/profile_search_page.dart
@@ -20,13 +20,7 @@ class _ProfileSearchState extends State<ProfileSearchPage> {
 
   //searchhistory with 5 last searched terms
   static const historyLength = 5;
-  static List<String> _searchHistory = [
-    "gunther",
-    "spast",
-    "boss",
-    "thb",
-    "mongo"
-  ];
+  static List<String> _searchHistory = ["gunther", "spast", "boss", "thb", "mongo"];
   List<String>? filteredSearchHistory;
   String? selectedTerm;
 
@@ -66,105 +60,108 @@ class _ProfileSearchState extends State<ProfileSearchPage> {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => ProfileSearchCubit(),
-      child: Scaffold(
-          body: FloatingSearchBar(
-              controller: controller,
-              body: FloatingSearchBarScrollNotifier(
-                child: SearchResultsListView(),
-              ),
-              transition: CircularFloatingSearchBarTransition(),
-              physics: BouncingScrollPhysics(),
-              title: Text(selectedTerm ?? "Search"),
-              hint: 'Search and find out',
-              actions: [
-                FloatingSearchBarAction.searchToClear(),
-              ],
-              onQueryChanged: (query) {
-                //search for old recommendations
-                setState(() {
-                  filteredSearchHistory = filterSearchTerms(filter: query);
-                });
-              },
-              onSubmitted: (query) async {
-                setState(() {
-                  if (query != "") {
-                    addSearchTerm(query);
-                    selectedTerm = query;
-                    //context.read<ProfileSearchCubit>().searchByProfileName(query);
-                  }
-                });
-              },
-              builder: (context, transition) {
-                return ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Material(
-                      color: Colors.white,
-                      elevation: 4,
-                      child: Builder(
-                        builder: (context) {
-                          //if query and history is empty, return the Start Searching Site
-                          if (filteredSearchHistory!.isEmpty &&
-                              controller.query.isEmpty) {
-                            return Container(
-                              height: 56,
-                              width: double.infinity,
-                              alignment: Alignment.center,
-                              child: Text(
-                                'Start Searching',
-                                maxLines: 1,
-                              ),
-                            );
-                          } else if (filteredSearchHistory!.isEmpty) {
-                            // if empty add query and show query as tile
-                            return ListTile(
-                              title: Text(controller.query),
-                              leading: const Icon(Icons.search),
-                              onTap: () {
-                                setState(() {
-                                  addSearchTerm(controller.query);
-                                  selectedTerm = controller.query;
-                                });
-                                controller.close();
-                              },
-                            );
-                          } else {
-                            //return the previous search history results
-                            return Column(
-                              children: filteredSearchHistory!
-                                  .map(
-                                    (term) => ListTile(
-                                  title: Text(
-                                    term,
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  leading: const Icon(Icons.history),
-                                  trailing: IconButton(
-                                    icon: const Icon(Icons.clear),
-                                    onPressed: () {
+      child: BlocBuilder<ProfileSearchCubit, ProfileSearchState>(
+          builder: (context, state){
+            return Scaffold(
+                body: FloatingSearchBar(
+                    controller: controller,
+                    body: FloatingSearchBarScrollNotifier(
+                      child: SearchResultsListView(),
+                    ),
+                    transition: CircularFloatingSearchBarTransition(),
+                    physics: BouncingScrollPhysics(),
+                    title: Text(selectedTerm ?? "Search"),
+                    hint: 'Search and find out',
+                    actions: [FloatingSearchBarAction.searchToClear(),],
+                    onQueryChanged: (query) {
+                      //search for old recommendations
+                      setState(() {
+                        filteredSearchHistory = filterSearchTerms(filter: query);
+                      });
+                    },
+                    onSubmitted: (query) async {
+                      setState(() {
+                        if (query != "") {
+                          addSearchTerm(query);
+                          selectedTerm = query;
+                          context.read<ProfileSearchCubit>().searchByProfileName(query);
+                        }
+                      });
+                    },
+                    builder: (context, transition) {
+                      return ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Material(
+                            color: Colors.white,
+                            elevation: 4,
+                            child: Builder(
+                              builder: (context) {
+                                //if query and history is empty, return the Start Searching Site
+                                if (filteredSearchHistory!.isEmpty &&
+                                    controller.query.isEmpty) {
+                                  return Container(
+                                    height: 56,
+                                    width: double.infinity,
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      'Start Searching',
+                                      maxLines: 1,
+                                    ),
+                                  );
+                                } else if (filteredSearchHistory!.isEmpty) {
+                                  // if empty add query and show query as tile
+                                  return ListTile(
+                                    title: Text(controller.query),
+                                    leading: const Icon(Icons.search),
+                                    onTap: () {
                                       setState(() {
-                                        deleteSearchTerm(term);
+                                        addSearchTerm(controller.query);
+                                        selectedTerm = controller.query;
                                       });
+                                      controller.close();
                                     },
-                                  ),
-                                  onTap: () {
-                                    setState(() {
-                                      putSearchTermFirst(term);
-                                      selectedTerm = term;
-                                    });
-                                    controller.close();
-                                  },
-                                ),
-                              )
-                                  .toList(),
-                            );
-                          }
-                        },
-                        //itemCount: this.profiles.length),
-                      ),
-                    ));
-                }
-                )),);
+                                  );
+                                } else {
+                                  //return the previous search history results
+                                  return Column(
+                                    children: filteredSearchHistory!
+                                        .map(
+                                          (term) => ListTile(
+                                        title: Text(
+                                          term,
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        leading: const Icon(Icons.history),
+                                        trailing: IconButton(
+                                          icon: const Icon(Icons.clear),
+                                          onPressed: () {
+                                            setState(() {
+                                              deleteSearchTerm(term);
+                                            });
+                                          },
+                                        ),
+                                        onTap: () {
+                                          setState(() {
+                                            putSearchTermFirst(term);
+                                            selectedTerm = term;
+                                          });
+                                          controller.close();
+                                        },
+                                      ),
+                                    )
+                                        .toList(),
+                                  );
+                                }
+                              },
+                              //itemCount: this.profiles.length),
+                            ),
+                          ));
+                    }
+                ));}
+
+      )
+      );
     }
 
   late FloatingSearchBarController controller;

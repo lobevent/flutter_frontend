7fbc7c317b50c9de1125700fc589d89d17fcb90c
prantto <t.prantz@gmx.de>
2021-07-07 19:23:29 +0200

    local config.dart changes

--- flutter_app/lib/data/local_config.dart
+++ flutter_app/lib/data/local_config.dart
@@ -1,1 +1,7 @@
-String adress = "";
+
+import 'dart:io';
+
+Platform.isIOS ? String url = "http://192.168.1.25:8000" : anOther(),
+
+static const String url = "http://192.168.1.25:8000";
+//static const String url = "http://localhost:8000";

a2b3da592120f8298fef87d3184aeea7bfba9b0e
Toni <toni.boeswetter@outlook.de>
2021-07-21 21:55:57 +0200

    local config push have fun

--- flutter_app/lib/data/local_config.dart
+++ flutter_app/lib/data/local_config.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
 
-
 //const String adress = "http://10.0.2.2:8000";
-const String adress = "http://192.168.1.16:8000";
+const String adress = "http://192.168.1.18:8000";

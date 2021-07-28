33c03735b770c1868c1ee401bfe156507a017f6e
Toni <toni.boeswetter@outlook.de>
2021-07-28 22:18:56 +0200

    merging

--- flutter_app/lib/presentation/pages/event/event_screen/widgets/todo_widget.dart
+++ flutter_app/lib/presentation/pages/event/event_screen/widgets/todo_widget.dart
@@ -4,22 +4,27 @@ import 'package:flutter_frontend/application/event/event_screen/event_screen_cub
 import 'package:flutter_frontend/presentation/pages/event/todos/event_todo_widget.dart';
 
 class TodoWidget extends StatelessWidget {
-
   const TodoWidget({Key? key}) : super(key: key);
+
+  Widget buildCreateTodoButton() {
+    return IconButton(onPressed: null, icon: Icon(Icons.add_circle));
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<EventScreenCubit, EventScreenState>(
         builder: (context, state) {
-          return state.maybeMap(
-              loaded: (state) {
-                return EventTodoWidget(todo: state.event.todo!);
-              },
-              orElse: () {
-                return const Text('');
-              },
-          );
-        }
-    );
+      return state.maybeMap(
+        loaded: (state) {
+          if (state.event.todo != null) {
+            return EventTodoWidget(todo: state.event.todo!);
+          } else
+            return buildCreateTodoButton();
+        },
+        orElse: () {
+          return const Text('');
+        },
+      );
+    });
   }
 }

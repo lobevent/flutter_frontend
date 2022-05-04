ff68a9b7fd6db7981a374fa3975ba3e05c3a8142
Daniel Lenhardt <ckwdani@gmail.com>
2022-05-04 22:10:30 +0200

    fix geolocator stuff

--- flutter_app/lib/application/core/geo_functions.dart
+++ flutter_app/lib/application/core/geo_functions.dart
@@ -1,11 +1,11 @@
-import 'package:flutter_frontend/infrastructure/core/position.dart';
-import 'package:geolocator/geolocator.dart';
-
-class GeoFunctions {
-  Future<Position> getLocation() async {
-    final Position location = await determinePosition();
-    // final double latitude = location.latitude;
-    // final double longitude = location.longitude;
-    return location;
-  }
-}
+// import 'package:flutter_frontend/infrastructure/core/position.dart';
+// import 'package:geolocator/geolocator.dart';
+//
+// class GeoFunctions {
+//   Future<Position> getLocation() async {
+//     final Position location = await determinePosition();
+//     // final double latitude = location.latitude;
+//     // final double longitude = location.longitude;
+//     return location;
+//   }
+// }

b44cb6bc6c529dce093ae3e1cb7790eeee11abcf
cupkuchen <sophiebuettner707@gmail.com>
2022-08-02 19:34:10 +0200

    event_form_container.dart mit Stepper
    Fehler im Rendering

--- flutter_app/lib/presentation/pages/event/event_form/widgets/event_form_container.dart
+++ flutter_app/lib/presentation/pages/event/event_form/widgets/event_form_container.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_frontend/domain/event/event.dart';
+import 'package:flutter_frontend/l10n/app_strings.dart';
 import 'package:flutter_frontend/presentation/pages/core/widgets/imageAndFiles/image_upload.dart';
 import 'package:flutter_frontend/presentation/pages/event/event_form/widgets/checkbox_area.dart';
 import 'package:flutter_frontend/presentation/pages/event/event_form/widgets/date_picker.dart';
@@ -14,7 +15,7 @@ import 'description_body_widged.dart';
 import 'event_series/add_to_series.dart';
 import 'title_widget.dart';
 
-class EventFormContainer extends StatelessWidget {
+class EventFormContainer extends StatefulWidget {
   final bool showErrorMessages;
   final bool isEditing;
   final Event? event;
@@ -26,112 +27,94 @@ class EventFormContainer extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<EventFormContainer> createState() => _EventFormContainerState();
+}
+
+class _EventFormContainerState extends State<EventFormContainer> {
+  int currentStep = 0;
+  bool isCompleted = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Row(
-    /*return Form(
+    return Form(
       // tell the form whether it should show error messages or not
-      autovalidateMode: showErrorMessages
+      autovalidateMode: widget.showErrorMessages
           ? AutovalidateMode.always
-          : AutovalidateMode.disabled,*/
-      children: [//Fehler?
-          DefaultTabController(
-            length: 4,
-            child: TabBar(
-              tabs: [
-                Tab(),//icons müssen noch hinzugefügt werden
-                Tab(),
-                Tab(),
-                Tab()
+          : AutovalidateMode.disabled,
+      child: Expanded(
+        child: Stepper(
+          type: StepperType.vertical,
+          steps: getSteps(),
+          currentStep: currentStep,
+          onStepContinue: () {
+            final isLastStep = currentStep == getSteps().length - 1;
+            if (isLastStep) {
+              setState(() => isCompleted = true);
+
+              ///send data to server
+            } else {
+              setState(() => currentStep += 1);
+            }
+          },
+          onStepTapped: (step) => setState(() => currentStep = step),
+          onStepCancel:
+              currentStep == 0 ? null : () => setState(() => currentStep -= 1),
+        ),
+      ),
+    );
+  }
+
+  List<Step> getSteps() => [
+        Step(
+          state: currentStep > 0 ? StepState.complete : StepState.indexed,
+          isActive: currentStep >= 0,
+          title: Text(AppStrings.createEventGeneralInfo),
+          content: Container(
+            child: Column(
+              children: [
+                if (widget.event != null && widget.event!.image != null)
+                  PickImageWidget(
+                    loadetFile: widget.event!.image,
+                  )
+                else
+                  const PickImageWidget(),
+
+                /// the input field, where the name is typed
+                const EventNameField(), // ATTENTION: the textfieldclasses have to be constant ( research has to be done into this! )
+
+                /// the input filed with the decription
+                const DescriptionField(),
               ],
             ),
           ),
-          Scaffold(
-              body: TabBarView(
-                children: [
-                  Form(
-                      autovalidateMode: showErrorMessages
-                          ? AutovalidateMode.always
-                          : AutovalidateMode.disabled,
-                      child:Column(
-                        children: [
-                        if (event != null && event!.image != null)
-                          PickImageWidget(
-                            loadetFile: event!.image,
-                          )
-                        else
-                          const PickImageWidget(),
-
-                        /// the input field, where the name is typed
-                        const EventNameField(), // ATTENTION: the textfieldclasses have to be constant ( research has to be done into this! )
-
-                        /// the input filed with the decription
-                        const DescriptionField(),
-                        ],
-                      ),
-                  ),
-                  Form(
-                    autovalidateMode: showErrorMessages
-                        ? AutovalidateMode.always
-                        : AutovalidateMode.disabled,
-                    child: Column(
-                      children: const [
-                        DatePicker(),
-
-                        CoordsPicker(),
-                      ],
-                    ),
-                  ),
-                  Form(
-                    autovalidateMode: showErrorMessages
-                        ? AutovalidateMode.always
-                        : AutovalidateMode.disabled,
-                    child: Column(
-                      children: [
-                        const CheckBoxArea(),
-
-                        const MaxPersons(),
-
-                        if (!isEditing) InviteFriendsWidget(),
-
-                        if (!isEditing) AddToSeries(),
-
-                        //const PickImageWidget(),
-                      ],
-                    )
-                  )
-                ],
-              ),
+        ),
+        Step(
+          state: currentStep > 1 ? StepState.complete : StepState.indexed,
+          isActive: currentStep >= 1,
+          title: Text(AppStrings.createEventPlaceTime),
+          content: Container(
+            child: Column(
+              children: const [
+                DatePicker(),
+                CoordsPicker(),
+              ],
             ),
-          ],
-            /*children: [
-              if (event != null && event!.image != null)
-                PickImageWidget(
-                  loadetFile: event!.image,
-                )
-              else
-                const PickImageWidget(),
-
-              /// the input field, where the name is typed
-              const EventNameField(), // ATTENTION: the textfieldclasses have to be constant ( research has to be done into this! )
-
-              /// the input filed with the decription
-              const DescriptionField(),
-
-
-              const DatePicker(),
-
-              const CoordsPicker(),
-
-              const CheckBoxArea(),
-
-              const MaxPersons(),
-
-              if (!isEditing) InviteFriendsWidget(),
-
-              if (!isEditing) AddToSeries(),
-
-              //const PickImageWidget(),
-            ],*/
-    );
-  }
+          ),
+        ),
+        Step(
+          state: currentStep > 2 ? StepState.complete : StepState.indexed,
+          isActive: currentStep >= 2,
+          title: Text(AppStrings.createEventAccess),
+          content: Expanded(
+            child: Column(
+              children: [
+                const CheckBoxArea(),
+                const MaxPersons(),
+                if (!widget.isEditing) InviteFriendsWidget(),
+                if (!widget.isEditing) AddToSeries(),
+              ],
+            ),
+          ),
+        ),
+      ];
 }

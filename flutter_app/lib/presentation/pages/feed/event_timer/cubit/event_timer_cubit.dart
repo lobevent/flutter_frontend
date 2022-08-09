d5e71bfcbea410a30497cbae556abf67bab9c44f
cupkuchen <sophiebuettner707@gmail.com>
2022-08-09 18:21:18 +0200

    event_timer cubit class im Feed

---
+++ flutter_app/lib/presentation/pages/feed/event_timer/cubit/event_timer_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'event_timer_state.dart';
+
+class EventTimerCubit extends Cubit<EventTimerState> {
+  EventTimerCubit() : super(EventTimerInitial());
+}
---
+++ flutter_app/lib/presentation/pages/feed/event_timer/cubit/event_timer_state.dart
@@ -0,0 +1,6 @@
+part of 'event_timer_cubit.dart';
+
+@immutable
+abstract class EventTimerState {}
+
+class EventTimerInitial extends EventTimerState {}

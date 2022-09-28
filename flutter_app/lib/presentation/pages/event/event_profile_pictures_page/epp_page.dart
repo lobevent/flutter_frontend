da26e2966f56dcb7285cefd47b2910f01722a3e1
Daniel Lenhardt <ckwdani@gmail.com>
2022-09-28 22:13:00 +0200

    add gridview animation

--- flutter_app/lib/presentation/pages/event/event_profile_pictures_page/epp_page.dart
+++ flutter_app/lib/presentation/pages/event/event_profile_pictures_page/epp_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_frontend/domain/event/event.dart';
 import 'package:flutter_frontend/presentation/pages/core/widgets/styling_widgets.dart';
 import 'package:flutter_frontend/presentation/pages/event/event_profile_pictures_page/cubit/epp_page_cubit.dart';
 import 'package:flutter_frontend/presentation/pages/event/event_profile_pictures_page/widgets/epp_page_grid_tile.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 ///
 /// Page to display Profile Event Images
@@ -30,14 +31,28 @@ class EPPPage extends StatelessWidget {
           isLoading: state is EppPLoading,
           child_ren: state.maybeMap(loaded:(lstate) => right(
             // ----------- gridview -----------
-              GridView.count(
-                mainAxisSpacing: 3.0,
-                crossAxisSpacing: 3.0,
-                padding: EdgeInsets.all(9.0),
-                crossAxisCount: (MediaQuery.of(context).size.width ~/ 100).toInt(),
-                children: lstate.epps.map((e) => EppGridListTile(epp: e,)).toList(),
+              AnimationLimiter(
+                child: GridView.count(
+                  mainAxisSpacing: 3.0,
+                  crossAxisSpacing: 3.0,
+                  padding: EdgeInsets.all(9.0),
+                  crossAxisCount: (MediaQuery.of(context).size.width ~/ 100).toInt(),
+                  children: List.generate(lstate.epps.length,
+                          (index) => AnimationConfiguration.staggeredGrid(
+                            position: index,
+                            duration: const Duration(milliseconds: 375),
+                            columnCount: (MediaQuery.of(context).size.width ~/ 100).toInt(),
+                            child: ScaleAnimation(
+                              child: FadeInAnimation(
+                                child: EppGridListTile(epp: lstate.epps[index],),
+                              ),
+                            ),
+                          )
+                  )
+                  //lstate.epps.map((e) => EppGridListTile(epp: e,)).toList(),
 
-        )), orElse: () { return right(Text("bklaa"));}),
+        ),
+              )), orElse: () { return right(Text("bklaa"));}),
         ),
       ),
     );
